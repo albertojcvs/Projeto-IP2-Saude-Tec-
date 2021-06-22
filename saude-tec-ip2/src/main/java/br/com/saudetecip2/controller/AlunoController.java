d21c4fbd3a185b6b88e62176c7563940361619a7
Alberto Jose <albertojcvs@gmail.com>
2021-06-22 01:33:53 -0300

    começando a tela de aluno

---
+++ saude-tec-ip2/src/main/java/br/com/saudetecip2/controller/AlunoController.java
@@ -0,0 +1,5 @@
+package br.com.saudetecip2.controller;
+
+public class AlunoController {
+
+}
--- saude-tec-ip2/src/main/java/br/com/saudetecip2/views/LoginAlunoController.java
+++ saude-tec-ip2/src/main/java/br/com/saudetecip2/views/LoginAlunoController.java
@@ -1,4 +1,5 @@
 package br.com.saudetecip2.views;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,46 +7,37 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.input.MouseEvent;
+
 public class LoginAlunoController {
 	@FXML
-    private Button botaoVoltar;
+	private Button botaoVoltar;
 
-    @FXML
-    private TextField campoIdAluno;
+	@FXML
+	private TextField campoIdAluno;
 
-    @FXML
-    private PasswordField campoSenha;
+	@FXML
+	private PasswordField campoSenha;
 
-    @FXML
-    private Button botaoEntrar;
+	@FXML
+	private Button botaoEntrar;
 
-    @FXML
-    void onBotaoEntrarClicked(MouseEvent event) {
-    		
-    }
+	@FXML
+	void onBotaoEntrarClicked(MouseEvent event) {
 
-    @FXML
-    void onBotaoVoltarClicked(MouseEvent event) {
-    	irParaHome();
-    }
-    
-    private void irParaHome() {
-    	try {
-    		Parent telaHome = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
-    		botaoVoltar.getScene().setRoot(telaHome);
-    	}
-    	catch(Exception e) {
-    		System.out.println("Erro ao ir para a Home!");
-    	}
-    }
-    
-    private void irParaAreaAluno() {
-    	try {
-    		Parent telaHome = FXMLLoader.load(getClass().getResource("/views/AreaAluno.fxml"));
-    		botaoVoltar.getScene().setRoot(telaHome);
-    	}
-    	catch(Exception e) {
-    		System.out.println("Erro ao ir para a Home!");
-    	}
-    }
+	}
+
+	@FXML
+	void onBotaoVoltarClicked(MouseEvent event) {
+		irParaHome();
+	}
+
+	private void irParaHome() {
+		try {
+			Parent telaHome = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
+			botaoVoltar.getScene().setRoot(telaHome);
+		} catch (Exception e) {
+			System.out.println("Erro ao ir para a Home!");
+		}
+	}
+
 }
