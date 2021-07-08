91c067924ebcc184423132b2e10b7006f0570346
Alberto José <49288213+albertojcvs@users.noreply.github.com>
2021-07-08 00:42:44 -0300

    configurando a tela de aulas agendadas do professor

--- saude-tec-ip2/src/main/java/br/com/saudetecip2/views/TelaAulasAgendadasProfessorController.java
+++ saude-tec-ip2/src/main/java/br/com/saudetecip2/views/TelaAulasAgendadasProfessorController.java
@@ -2,23 +2,38 @@ package br.com.saudetecip2.views;
 
 import java.net.URL;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Observable;
 import java.util.ResourceBundle;
 
+import br.com.saudetecip2.controller.Aulacontroller;
+import br.com.saudetecip2.controller.LoginFuncionarioController;
 import br.com.saudetecip2.domain.enums.TipoDeAula;
 import br.com.saudetecip2.domain.enums.TipoDeTreino;
 import br.com.saudetecip2.domain.model.Aula;
-
+import br.com.saudetecip2.domain.model.Funcionario;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
 import javafx.scene.input.MouseEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 
 public class TelaAulasAgendadasProfessorController implements Initializable {
 
-    @FXML
+    Aulacontroller aulaController = new Aulacontroller();
+	
+    Funcionario professorLogado;
+    
+    LoginFuncionarioController loginFuncionarioController = LoginFuncionarioController.getInstace();
+    
+	@FXML
     private TableView<Aula> tabelaDeAulas;
 
     @FXML
@@ -32,11 +47,44 @@ public class TelaAulasAgendadasProfessorController implements Initializable {
 
     @FXML
     private Text textoNome;
-
+    
+    
+    private void preencherTabelasDeAula() {
+    	List<Aula> aulas = aulaController.buscarAulasDoProfessor(professorLogado.getId());
+    	
+    	ObservableList listaDeAulas = FXCollections.observableArrayList(aulas);
+    	tabelaDeAulas.setItems(listaDeAulas);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    	
+    	colunaData.setCellFactory(cell -> {
+			return new TableCell<Aula, Timestamp>() {
+				SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy - HH:mm");
+
+				@Override
+				protected void updateItem(Timestamp item, boolean empty) {
+					super.updateItem(item, empty);
+					if (!empty) {
+						setText(format.format(item));
+					} else {
+						setText("");
+						setGraphic(null);
+					}
+				}
+			};
+		});
     
+    	colunaData.setCellValueFactory(new PropertyValueFactory<>("Data"));
+    	colunaTipoDeAula.setCellValueFactory(new PropertyValueFactory<>("TipoDeAula"));
+    	colunaTipoDeTreino.setCellValueFactory(new PropertyValueFactory<>("TipoDeTreino"));
+    	
+    	
+    	professorLogado = loginFuncionarioController.getFuncionarioLogado();
+    	
+    	textoNome.setText(professorLogado.getNome());
+    	
     }
     	
     
